c5ab9f59b3f57929df29209c697d3e23100a1c1c
Pranjal Kumar <pranjalk449@gmail.com>
2024-09-11 00:05:48 +0530

    blog web app

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const Blog = require("./models/blog");
+const app = express();
+
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URL)
+        .then(e=>console.log("MongoDB Connected"));
+
+
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
+app.get("/", async (req,res)=>{
+  const allBlogs =  await Blog.find({});
+    res.render("home",{
+      user: req.user,
+      blogs:allBlogs,
+    });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.resolve("./public")));
+
+app.listen(PORT,()=>{
+  console.log(`Server started at PORT: ${PORT}`);
+});
