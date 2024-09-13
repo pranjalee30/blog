6efc83057d178835b7538ad9e35baaf3a5d4dd61
Pranjal Kumar <pranjalk449@gmail.com>
2024-09-13 20:42:25 +0530

    profile image URL updated

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
     },
     profileImageURL: {
       type: String,
-      default: "/images/default.png",
+      default: "/images/Avatar.svg",
     },
     role: {
       type: String,
